92f73e45ef59ca519c9f0f937f6345f1065cb8f9
BabyDolphiner <clover.dolphin@outlook.com>
2017-05-19 14:42:58 +0800

    the first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//this is my first project
+/* 据说这可以多行注释
+        *看来真的可以哎，都忘得差不多了，好尴尬
+        */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
